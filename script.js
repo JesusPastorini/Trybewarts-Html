0bd0f659ed38f52d2dc7b28c3dbfaf230f6005b9
JesusPastorini <juniorpastorini_@hotmail.com>
2023-02-15 15:08:01 -0300

    Iniciando projeto adiantado

---
+++ script.js
@@ -0,0 +1,9 @@
+const verificaloginsenha = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (email === 'tryber@teste.com' && password === '123456') {
+        alert('Olá, Tryber!')
+    } else {
+        alert('Email ou senha inválidos.')
+    }
+}
