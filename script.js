0b4e272e7b3fc7b35c145938a62f3e989f2911e3
Vitor Gomes <vitorgomes14@hotmail.com>
2023-02-15 15:58:29 -0300

    Acrescentados os requisitos 12 ao 19 adiantados

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ const verificaloginsenha = () => {
         alert('Email ou senha inválidos.')
     }
 }
+
+const labelRate = document.getElementById('label-rate');
+
+for (i = 0; i < 6; i += 1) {
+    const values = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
+    const checkContent = document.createElement('input');
+    checkContent.type = 'checkbox';
+    checkContent.className = 'subject';
+    checkContent.value = values[i];
+    const contentOptionText = document.createElement('span');
+    contentOptionText.innerText = values[i];
+    labelRate.parentNode.insertBefore(checkContent, labelRate);
+    labelRate.parentNode.insertBefore(contentOptionText, labelRate);
+}
+
+const textArea = document.getElementsByClassName('textarea')[0];
+
+for (i = 1; i <= 10; i += 1) {
+    const inputSatisfaction = document.createElement('input');
+    inputSatisfaction.type = 'radio';
+    inputSatisfaction.value = i;
+    inputSatisfaction.name = 'rate';
+    const rate = document.createElement('span');
+    rate.innerText = i;
+    textArea.parentNode.insertBefore(inputSatisfaction, textArea);
+    textArea.parentNode.insertBefore(rate, textArea);
+}
+
+const submitBtn = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
+
+agreementCheck = () => {
+    if (agreement.checked === true) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+}
+
+agreement.addEventListener('change', agreementCheck);
+
+window.onload = agreementCheck;
